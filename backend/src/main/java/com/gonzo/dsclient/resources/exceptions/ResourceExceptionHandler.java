2baf8295b8ac8aa28c916be298c808ff444954d6
Gilnei <gilbegprog@gmail.com>
2020-10-07 17:03:24 -0300

    PUT Client(Update)

--- backend/src/main/java/com/gonzo/dsclient/resources/exceptions/ResourceExceptionHandler.java
+++ backend/src/main/java/com/gonzo/dsclient/resources/exceptions/ResourceExceptionHandler.java
@@ -9,13 +9,13 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
-import com.gonzo.dsclient.services.exceptions.EntityNotFoundException;
+import com.gonzo.dsclient.services.exceptions.ResourceNotFoundException;
 
 @ControllerAdvice
 public class ResourceExceptionHandler {
 
-	@ExceptionHandler(EntityNotFoundException.class)
-	public ResponseEntity<StandardError> entitiesNotFound(EntityNotFoundException e, HttpServletRequest request){
+	@ExceptionHandler(ResourceNotFoundException.class)
+	public ResponseEntity<StandardError> entitiesNotFound(ResourceNotFoundException e, HttpServletRequest request){
 		HttpStatus status = HttpStatus.NOT_FOUND;		
 		StandardError err = new StandardError();
 		err.setTimestamp(Instant.now());
