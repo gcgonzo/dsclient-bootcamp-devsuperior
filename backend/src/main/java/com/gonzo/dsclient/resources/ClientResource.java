0170ce3e25324e6ccc5bfdcff2d65328257c40ba
Gilnei <gilbegprog@gmail.com>
2020-10-07 16:37:42 -0300

    POST new Client

--- backend/src/main/java/com/gonzo/dsclient/resources/ClientResource.java
+++ backend/src/main/java/com/gonzo/dsclient/resources/ClientResource.java
@@ -1,16 +1,19 @@
 package com.gonzo.dsclient.resources;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.gonzo.dsclient.dto.ClientDTO;
-import com.gonzo.dsclient.entities.Client;
 import com.gonzo.dsclient.services.ClientService;
 
 @RestController
@@ -31,4 +34,16 @@ public class ClientResource {
 		ClientDTO dto = service.findById(id);
 		return ResponseEntity.ok().body(dto);
 	}
+	
+	@PostMapping
+	public ResponseEntity<ClientDTO> insert(@RequestBody ClientDTO dto){
+		dto = service.insert(dto);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
+				.buildAndExpand(dto.getId()).toUri();
+		return ResponseEntity.created(uri).body(dto);
+		
+		
+	}
+	
+	
 }
