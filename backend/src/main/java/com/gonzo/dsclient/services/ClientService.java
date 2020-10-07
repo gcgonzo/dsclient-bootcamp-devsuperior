21ab317ea6bf1f9afde9b26a9c284b61eba60683
Gilnei <gilbegprog@gmail.com>
2020-10-07 19:17:49 -0300

    Pagination

--- backend/src/main/java/com/gonzo/dsclient/services/ClientService.java
+++ backend/src/main/java/com/gonzo/dsclient/services/ClientService.java
@@ -1,14 +1,14 @@
 package com.gonzo.dsclient.services;
 
-import java.util.List;
 import java.util.Optional;
-import java.util.stream.Collectors;
 
 import javax.persistence.EntityNotFoundException;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -27,10 +27,9 @@ public class ClientService {
 	
 	
 	@Transactional(readOnly = true)
-	public List<ClientDTO>findAll(){
-		List<Client> list = repository.findAll();
-		
-		return list.stream().map(x -> new ClientDTO(x)).collect(Collectors.toList());
+	public Page<ClientDTO>findAllPaged(PageRequest pageRequeste){
+		Page<Client> list = repository.findAll(pageRequeste);
+		return list.map(x -> new ClientDTO(x));
 		
 	}
 
